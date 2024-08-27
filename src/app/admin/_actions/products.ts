f5a9001c88a1e28c9d3717a7d9f0ec858030646b
PieMenard <pinedo.m.gustavo@gmail.com>
2024-08-27 00:18:16 -0400

    add product card list to homepage

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -53,30 +53,34 @@ export async function addProduct(prevState: unknown, formData: FormData) {
 
 const editSchema = addSchema.extend({
   file: fileSchema.optional(),
-  image: imageSchema.optional()
-})
+  image: imageSchema.optional(),
+});
 
-export async function updateProduct(id: string, prevState: unknown, formData: FormData) {
+export async function updateProduct(
+  id: string,
+  prevState: unknown,
+  formData: FormData
+) {
   const result = editSchema.safeParse(Object.fromEntries(formData.entries()));
   if (!result.success) {
     return result.error.formErrors.fieldErrors;
   }
 
   const data = result.data;
-  const product = await prisma.product.findUnique({ where: { id } })
+  const product = await prisma.product.findUnique({ where: { id } });
 
-  if (product === null) return notFound()
+  if (product === null) return notFound();
 
-  let filePath = product.filePath
+  let filePath = product.filePath;
   if (data.file != null && data.file.size > 0) {
-    await fs.unlink(product.filePath)
+    await fs.unlink(product.filePath);
     filePath = `products/${crypto.randomUUID()}-${data.file.name}`;
     await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer()));
   }
 
-  let imagePath = product.imagePath
+  let imagePath = product.imagePath;
   if (data.image != null && data.image.size > 0) {
-    await fs.unlink(product.imagePath)
+    await fs.unlink(product.imagePath);
     imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`;
     await fs.writeFile(
       `public${imagePath}`,
@@ -123,6 +127,6 @@ export async function deleteProduct(id: string) {
     return notFound();
   }
 
-  await fs.unlink(product.filePath)
-  await fs.unlink(`public${product.imagePath}`)
+  await fs.unlink(product.filePath);
+  await fs.unlink(`public${product.imagePath}`);
 }
