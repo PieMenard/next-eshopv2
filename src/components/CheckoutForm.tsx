43f4f04bdb4dace385c07a52a159cf0ecf4b0e29
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-08-29 14:52:29 -0400

    style checkout form

--- src/components/CheckoutForm.tsx
+++ src/components/CheckoutForm.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { formatCurrency } from '@/lib/formatters';
 import { Product } from '@prisma/client';
 import {
   Elements,
@@ -8,6 +9,16 @@ import {
   useStripe,
 } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
+import Image from 'next/image';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './ui/card';
+import { Button } from './ui/button';
 
 const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string
@@ -21,16 +32,58 @@ const CheckoutForm = ({
   clientSecret: string;
 }) => {
   return (
-    <Elements options={{ clientSecret }} stripe={stripePromise}>
-      <Form />
-    </Elements>
+    <div className="max-w-5xl w-full mx-auto space-y-8">
+      <div className="flex gap-4 items-center">
+        <div className="aspect-video flex-shrink-0 w-1/3 relative">
+          <Image
+            src={product.imagePath}
+            fill
+            alt={product.name}
+            className="object-cover"
+          />
+        </div>
+        <div>
+          <div className="text-xl">
+            {formatCurrency(product.priceInCents / 100)}
+          </div>
+          <h1 className="text-2xl font-bold">{product.name}</h1>
+          <div className="line-clamp-3 text-muted-foreground">
+            {product.description}
+          </div>
+        </div>
+      </div>
+      <Elements options={{ clientSecret }} stripe={stripePromise}>
+        <Form priceInCents={product.priceInCents} />
+      </Elements>
+    </div>
   );
 };
 
 export default CheckoutForm;
 
-function Form() {
+function Form({ priceInCents }: { priceInCents: number }) {
   const stripe = useStripe();
   const elements = useElements();
-  return <PaymentElement />;
+  return (
+    <form>
+      <Card>
+        <CardHeader>
+          <CardTitle>Checkout</CardTitle>
+          <CardDescription className="text-destructive">Error</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <PaymentElement />
+        </CardContent>
+        <CardFooter>
+          <Button
+            className="w-full"
+            size="lg"
+            disabled={stripe == null || elements == null}
+          >
+            Purchase - {formatCurrency(priceInCents / 100)}
+          </Button>
+        </CardFooter>
+      </Card>
+    </form>
+  );
 }
