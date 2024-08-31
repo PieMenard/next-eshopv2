fdb590c0118ead34fef9413e57a46e05fc507801
PieMenard <pinedo.m.gustavo@gmail.com>
2024-08-31 00:33:44 -0400

    fix typos

--- src/app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
+++ src/app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
@@ -1,36 +1,41 @@
-import { prisma } from "@/db/db";
-import { NextRequest, NextResponse } from "next/server";
-import fs from 'fs/promises'
+import { prisma } from '@/db/db';
+import { NextRequest, NextResponse } from 'next/server';
+import fs from 'fs/promises';
 
-export async function Get(req: NextRequest,
-    { params: { downloadVerificationId } }: { params: { downloadVerificationId: string } }
+export async function GET(
+  req: NextRequest,
+  {
+    params: { downloadVerificationId },
+  }: { params: { downloadVerificationId: string } }
 ) {
-    const data = await prisma.downloadVerification.findUnique({
-        where: {
-            id: downloadVerificationId,
-            expiresAt: { gt: new Date() }
-        },
+  const data = await prisma.downloadVerification.findUnique({
+    where: {
+      id: downloadVerificationId,
+      expiresAt: { gt: new Date() },
+    },
+    select: {
+      product: {
         select: {
-            product: {
-                select: {
-                    filePath: true,
-                    name: true
-                }
-            }
-        }
-    })
+          filePath: true,
+          name: true,
+        },
+      },
+    },
+  });
 
-    if (!data) {
-        return NextResponse.redirect(new URL("/products/download/expired", req.url))
-    }
-    const { size } = await fs.stat(data.product.filePath)
-    const file = await fs.readFile(data.product.filePath)
-    const extension = data.product.filePath.split(".").pop()
+  if (!data) {
+    return NextResponse.redirect(
+      new URL('/products/download/expired', req.url)
+    );
+  }
+  const { size } = await fs.stat(data.product.filePath);
+  const file = await fs.readFile(data.product.filePath);
+  const extension = data.product.filePath.split('.').pop();
 
-    return new NextResponse(file, {
-        headers: {
-            "Content-Disposition": `attachment; filename="${data.product.name}.${extension}"`,
-            "Content-Lenght": size.toString()
-        }
-    })
+  return new NextResponse(file, {
+    headers: {
+      'Content-Disposition': `attachment; filename="${data.product.name}.${extension}"`,
+      'Content-Length': size.toString(),
+    },
+  });
 }
