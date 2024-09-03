a814771e093ffb9b95b26a9c83857af7db408021
PieMenard <pinedo.m.gustavo@gmail.com>
2024-09-02 21:45:19 -0400

    add admin sales page

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -13,8 +13,8 @@ const AdminLayout = ({
       <Nav>
         <NavLink href="/admin">DashBoard</NavLink>
         <NavLink href="/admin/products">Products</NavLink>
-        <NavLink href="/users">Costumers</NavLink>
-        <NavLink href="/orders">Sales</NavLink>
+        <NavLink href="/admin/users">Costumers</NavLink>
+        <NavLink href="/admin/orders">Sales</NavLink>
       </Nav>
       <div className="container my-6">{children}</div>
     </>
